1392eb194d4cee980102fcffd7c6306cf30e1309
wyman <wangyin@chinazyjr.com>
2017-10-24 10:56:49 +0800

    侧滑删除

--- app/src/main/java/com/zyjr/emergencylending/ui/home/MessageActivity.java
+++ app/src/main/java/com/zyjr/emergencylending/ui/home/MessageActivity.java
@@ -2,7 +2,6 @@ package com.zyjr.emergencylending.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -11,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ajguan.library.EasyRefreshLayout;
+import com.ajguan.library.LoadModel;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.zyjr.emergencylending.R;
 import com.zyjr.emergencylending.adapter.SwipeAdapter;
@@ -31,7 +31,7 @@ import butterknife.OnClick;
  * Created by wangyin on 2017/10/11.
  */
 
-public class MessageActivity extends BaseActivity {
+public class MessageActivity extends BaseActivity implements SwipeAdapter.onSwipeListener, EasyRefreshLayout.EasyEvent, BaseQuickAdapter.OnItemChildClickListener {
 
     @BindView(R.id.iv_back)
     ImageView ivBack;
@@ -41,7 +41,7 @@ public class MessageActivity extends BaseActivity {
     RecyclerView rvMain;
     @BindView(R.id.easylayout)
     EasyRefreshLayout easylayout;
-
+    SwipeAdapter myAdapter;
 
     private int pageNum = 1;
     private int pageSize;
@@ -63,95 +63,25 @@ public class MessageActivity extends BaseActivity {
 
     private void init() {
 
+
+        easylayout.setLoadMoreModel(LoadModel.NONE);
+        easylayout.addEasyEvent(this);
         messageBeanList = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
             MessageBean messageBean = new MessageBean("test" + i, new Date().toString(), "wangyin" + i);
             messageBeanList.add(messageBean);
         }
-        rvMain = (RecyclerView) findViewById(R.id.rv_main);
-        easylayout = (EasyRefreshLayout) findViewById(R.id.easylayout);
-
-        final SwipeAdapter myAdapter = new SwipeAdapter(R.layout.item_message_swipe, messageBeanList);
+        myAdapter = new SwipeAdapter(R.layout.item_message_swipe, messageBeanList);
 
         rvMain.setLayoutManager(new LinearLayoutManager(this));
         rvMain.setAdapter(myAdapter);
-//        myAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-//                Toast.makeText(MessageActivity.this, position + "a", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-        myAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
-            @Override
-            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
-
-                startActivity(new Intent(mContext, MessageDetail.class));
-            }
-        });
-//        myAdapter.addHeaderView(View.inflate(this, R.layout.header, null));
-        myAdapter.setOnDelListener(new SwipeAdapter.onSwipeListener() {
-            @Override
-            public void onDel(int pos) {
-                Toast.makeText(MessageActivity.this, pos + "c", Toast.LENGTH_SHORT).show();
-                myAdapter.getData().remove(pos - 1);
-                myAdapter.notifyItemRemoved(pos);//推荐用这个
-                if (pos != (myAdapter.getData().size())) { // 如果移除的是最后一个，忽略 注意：这里的mDataAdapter.getDataList()不需要-1，因为上面已经-1了
-                    myAdapter.notifyItemRangeChanged(pos, myAdapter.getData().size() - pos);
-                }
-            }
-        });
-        easylayout.addEasyEvent(new EasyRefreshLayout.EasyEvent() {
-            @Override
-            public void onLoadMore() {
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        final List<MessageBean> list = new ArrayList<>();
-                        for (int i = 0; i < 10; i++) {
-                            MessageBean messageBean = new MessageBean("test" + i, new Date().toString(), "wangyin" + i);
-                            messageBeanList.add(messageBean);
-                        }
-
-//                        myAdapter.addData(mList);
-
-                        easylayout.loadMoreComplete(new EasyRefreshLayout.Event() {
-                            @Override
-                            public void complete() {
-                                myAdapter.addData(list);
-                                myAdapter.getData().addAll(list);
-                                myAdapter.notifyDataSetChanged();
-
-                            }
-                        }, 500);
-
-                    }
-                }, 2000);
-
-
-            }
-
-            @Override
-            public void onRefreshing() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        List<MessageBean> list = new ArrayList<>();
-                        for (int i = 0; i < 5; i++) {
-                            MessageBean user = new MessageBean("test" + i, new Date().toString(), "wangyin" + i);
-                            list.add(user);
-                        }
-                        myAdapter.setNewData(list);
-                        easylayout.refreshComplete();
-                        Toast.makeText(getApplicationContext(), "refresh success", Toast.LENGTH_SHORT).show();
-                    }
-                }, 1000);
-
-            }
-        });
+        myAdapter.setOnItemChildClickListener(this);
+        myAdapter.setOnDelListener(this);
 
 
     }
+
     @OnClick({R.id.iv_back, R.id.tv_mark})
     public void onViewClicked(View view) {
         switch (view.getId()) {
@@ -176,5 +106,30 @@ public class MessageActivity extends BaseActivity {
 
                 break;
         }
+    }
+
+    @Override
+    public void onDel(int pos) {
+        Toast.makeText(MessageActivity.this, pos + "c", Toast.LENGTH_SHORT).show();
+        myAdapter.getData().remove(pos);
+        myAdapter.notifyItemRemoved(pos);
+        if (pos != (myAdapter.getData().size())) {
+            myAdapter.notifyItemRangeChanged(pos, myAdapter.getData().size() - pos);
+        }
+    }
+
+    @Override
+    public void onLoadMore() {
+
+    }
+
+    @Override
+    public void onRefreshing() {
+        easylayout.refreshComplete();
+    }
+
+    @Override
+    public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+        startActivity(new Intent(mContext, MessageDetail.class));
     }
 }
