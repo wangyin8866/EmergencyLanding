22192760e2077e65a6d13ded0b3db8b9e3e4032a
wyman <wangyin@chinazyjr.com>
2017-10-27 14:21:10 +0800

    登录注册接口

---
+++ app/src/main/java/com/zyjr/emergencylending/WBShareActivity.java
@@ -0,0 +1,13 @@
+package com.zyjr.emergencylending;
+
+//import com.umeng.socialize.media.WBShareCallBackActivity;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+/**
+ *
+ * @author wangfei
+ * @date 15/12/3
+ */
+public class WBShareActivity extends WBShareCallBackActivity {
+}
---
+++ app/src/main/java/com/zyjr/emergencylending/wxapi/WXEntryActivity.java
@@ -0,0 +1,15 @@
+package com.zyjr.emergencylending.wxapi;
+
+
+//import com.umeng.weixin.callback.WXCallbackActivity;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * @author wangyin
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+
+}
