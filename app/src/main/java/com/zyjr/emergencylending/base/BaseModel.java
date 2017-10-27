049183ca8a3bf514eda99ea2034f2dd9976a2626
fish19920404 <136687211@qq.com>
2017-10-27 18:06:46 +0800

    添加通用返回实体，优化借款金额选取

--- app/src/main/java/com/zyjr/emergencylending/base/BaseModel.java
+++ app/src/main/java/com/zyjr/emergencylending/base/BaseModel.java
@@ -9,11 +9,16 @@ import com.zyjr.emergencylending.utils.LogUtils;
 import com.zyjr.emergencylending.utils.SPUtils;
 import com.zyjr.emergencylending.utils.WYUtils;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.FormBody;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -38,16 +43,31 @@ public class BaseModel {
     private OkHttpClient.Builder httpClientBuilder;
 
     protected BaseModel() {
-        BasicParamsInterceptor basicParamsInterceptor = new BasicParamsInterceptor.Builder()
-                .addQueryParam("juid", SPUtils.getString(BaseApplication.getContext(), Config.KEY_JUID, ""))
-                .addQueryParam("login_token", SPUtils.getString(BaseApplication.getContext(), Config.KEY_TOKEN, ""))
-                .addQueryParam("version_no", Constants.getVersionCode(BaseApplication.getContext()))
-                .addQueryParam("register_platform", Constants.getPlatform(1))
-                .build();
         //手动创建一个OkHttpClient并设置超时时间
         httpClientBuilder = new OkHttpClient.Builder();
         if (WYUtils.isApkInDebug(BaseApplication.getContext())) {
-            httpClientBuilder.retryOnConnectionFailure(true).addInterceptor(basicParamsInterceptor).addInterceptor(new LogInterceptor()).connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+            httpClientBuilder.retryOnConnectionFailure(true).addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request original = chain.request();
+                    Request.Builder requestBuilder = original.newBuilder();
+                    //请求体定制：统一添加定制参数
+                    if(original.body() instanceof FormBody){
+                        FormBody.Builder newFormBody = new FormBody.Builder();
+                        FormBody oidFormBody = (FormBody) original.body();
+                        for (int i = 0;i<oidFormBody.size();i++){
+                            newFormBody.addEncoded(oidFormBody.encodedName(i),oidFormBody.encodedValue(i));
+                        }
+                        newFormBody.add("juid",SPUtils.getString(BaseApplication.getContext(), Config.KEY_JUID, "e517fafd0d4a4034b4a88a6a1e041540"));
+                        newFormBody.add("login_token",SPUtils.getString(BaseApplication.getContext(), Config.KEY_TOKEN, "login_token"));
+                        newFormBody.add("version_no", Constants.getVersionCode(BaseApplication.getContext()));
+                        newFormBody.add("register_platform",Constants.getPlatform(1));
+                        requestBuilder.method(original.method(),newFormBody.build());
+                    }
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                }
+            }).addInterceptor(new LogInterceptor()).connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
         } else {
             httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
         }
